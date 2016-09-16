769f7792a718cccf75fd1661b35f01d5a231306b
Tonia Hsia <toniahsia@Tonias-MBP.hsd1.ca.comcast.net>
2016-09-16 00:04:38 -0700

    create Stack class and functions

--- W1D4/stack.rb
+++ W1D4/stack.rb
@@ -1,13 +1,18 @@
 class Stack
+  attr_reader :stack_arr
+
   def initialize
+    @stack_arr = []
   end
 
-  def add(el)
+  def add(*els)
+    els.each { |el| @stack_arr.push(el) }
   end
 
   def remove
+    puts "The stack is already empty!" if @stack_arr.empty?
+    @stack_arr.pop
   end
 
-  def show
-  end
+  alias_method :show, :stack_arr
 end
