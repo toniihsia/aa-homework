fa6eedaf91fe1c51ab043dfa97e0e74ba4363bc9
toniihsia <tonia.hsia@gmail.com>
2016-11-28 22:49:43 -0800

    heap

--- W12/heap/lib/heap.rb
+++ W12/heap/lib/heap.rb
@@ -1,33 +1,97 @@
+require 'byebug'
+
 class BinaryMinHeap
   def initialize(&prc)
-    bloop
+    @store = []
+    @prc = prc || Proc.new { |el1, el2| el1 <=> el2 }
   end
 
   def count
+    @store.length
   end
 
   def extract
+    raise "This heap is empty." if @store.length <= 0
+
+    popped_val = @store[0]
+    if count > 1
+      @store[0] = @store.pop
+      self.class.heapify_down(@store, 0, &prc)
+    else
+      @store.pop
+    end
+
+    popped_val
   end
 
+
   def peek
+    raise "This heap is empty." if @store.length <= 0
+    @store[0]
   end
 
   def push(val)
+    @store << val
+    self.class.heapify_up(@store, self.count - 1, &prc)
   end
 
   protected
   attr_accessor :prc, :store
 
   public
-  def self.child_indices(len, parent_index)
+  def self.child_indices(length, parent_index)
+      ughh = []
+      left_child_idx = ((2 * parent_index) + 1) unless ((2 * parent_index) + 1) >= length
+      right_child_idx = ((2 * parent_index) + 2) unless ((2 * parent_index) + 2) >= length
+
+      ughh << left_child_idx if left_child_idx
+      ughh << right_child_idx if right_child_idx
+
+      return ughh
   end
 
   def self.parent_index(child_index)
+    raise "root has no parent" if child_index == 0
+    (child_index - 1) / 2
   end
 
-  def self.heapify_down(array, parent_idx, len = array.length, &prc)
+  def self.heapify_down(array, parent_idx, length = array.length, &prc)
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+    left_child_idx, right_child_idx = child_indices(length, parent_idx)
+
+    parent_val = array[parent_idx]
+
+    children = []
+    children << array[left_child_idx] if left_child_idx
+    children << array[right_child_idx] if right_child_idx
+
+    if children.all? { |child| prc.call(parent_val, child) <= 0 }
+      return array
+    end
+
+    swap_idx = nil
+    if children.length == 1
+      swap_idx = left_child_idx
+    else
+      swap_idx = prc.call(children[0], children[1]) == -1 ? left_child_idx : right_child_idx
+    end
+
+    array[parent_idx], array[swap_idx] = array[swap_idx], parent_val
+    heapify_down(array, swap_idx, length, &prc)
   end
 
-  def self.heapify_up(array, child_idx, len = array.length, &prc)
+  def self.heapify_up(array, child_idx, length = array.length, &prc)
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+
+    return array if child_idx <= 0
+
+    parent_idx = parent_index(child_idx)
+    child_val, parent_val = array[child_idx], array[parent_idx]
+    if prc.call(child_val, parent_val) >= 0
+      return array
+    else
+      array[child_idx], array[parent_idx] = parent_val, child_val
+      heapify_up(array, parent_idx, length, &prc)
+    end
   end
 end
--- W12/heap/lib/heap_sort.rb
+++ W12/heap/lib/heap_sort.rb
@@ -2,5 +2,15 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    (2..self.length).each do |heap_size|
+      BinaryMinHeap.heapify_up(self, heap_size - 1, heap_size)
+    end
+
+    self.length.downto(2).each do |heap_size|
+      self[heap_size - 1], self[0] = self[0], self[heap_size - 1]
+      BinaryMinHeap.heapify_down(self, 0, heap_size - 1)
+    end
+
+    self.reverse!
   end
 end
